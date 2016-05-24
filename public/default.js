f2543e6248905dde3ceb2a679c1e5e8b54b34621
Marcel Molenaar <Marcel Molenaar>
2016-05-24 15:05:22 +0200

    Initial version

---
+++ public/default.js
@@ -0,0 +1,33 @@
+var board;
+var gamel
+var socket = io();
+
+window.onload = function () {
+    initGame();
+};
+
+var initGame = function () {
+    var cfg = {
+        draggable: true,
+        position: 'start',
+        onDrop: handleMove,
+    };
+    board = new ChessBoard('gameBoard', cfg);
+    game = new Chess();
+};
+
+
+// called when a player makes a move on the board UI
+var handleMove = function(source, target) {
+    var move = game.move({from: source, to: target});
+
+
+if (move === null) return 'snapback';
+    else socket.emit('move', move);
+};
+
+// called when the server calls socket broadcast('move')
+socket.on('move', function (msg) {
+   game.move(msg);
+   board.position(game.fen());  // fen is the board layout 
+});
